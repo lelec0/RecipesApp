de9092392a09c7498b79d66a79fbc509476b5136
Laura <la_12_msn@hotmail.com>
2022-01-27 18:27:25 -0300

    fix: card container

--- src/components/Card/index.js
+++ src/components/Card/index.js
@@ -5,14 +5,12 @@ function Card(objectValues) {
   const { source, alt, title, test } = objectValues;
   return (
     <CardContainer>
-      <div>
-        <img
-          data-testid={ test }
-          src={ source }
-          alt={ alt }
-        />
-        <h1>{title}</h1>
-      </div>
+      <img
+        data-testid={ test }
+        src={ source }
+        alt={ alt }
+      />
+      <h1>{title}</h1>
     </CardContainer>
   );
 }
