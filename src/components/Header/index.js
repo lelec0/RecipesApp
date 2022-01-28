0ebf6bb65ddb8b0b5eca488236c4274dcefc7256
Juliane Marques <mqs.juliane@gmail.com>
2022-01-28 16:24:54 -0300

    feat: header req-9

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -3,7 +3,22 @@ import React from 'react';
 function Header() {
   return (
     <div>
-      Header
+      <div
+        data-testid="profile-top-btn"
+      >
+        perfil
+      </div>
+      <div
+        data-testid="page-title"
+      >
+        titulo
+      </div>
+      <button
+        type="button"
+        data-testid="search-top-btn"
+      >
+        botao
+      </button>
     </div>
   );
 }
