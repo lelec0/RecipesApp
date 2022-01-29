a9a7e698dfeb4e10656d644e7d7fc15caf1b4610
Juliane Marques <mqs.juliane@gmail.com>
2022-01-29 16:29:02 -0300

    feat: adding first test of food page (header)

--- src/tests/foods.test.js
+++ src/tests/foods.test.js
@@ -1,13 +1,25 @@
 import React from 'react';
-import { render, screen } from '@testing-library/react';
+import { screen } from '@testing-library/react';
 import userEvent from '@testing-library/user-event';
 import '@testing-library/jest-dom';
-import App from '../App';
 import renderWithRouter from '../helpers/renderWithRouter';
+import Foods from '../pages/Foods/index';
+import searchIcon from '../assets/images/searchIcon.svg';
+import profileIcon from '../assets/images/profileIcon.svg';
 
-describe('Foods Page', () => {
-  it('has the header component', () => {
-    
+describe('1 - Foods Page Header', () => {
+  it('verifies search icon, user icon and page title', () => {
+    renderWithRouter(<Foods />);
+    const magnifierIcon = screen.getByRole('img');
+    const userIcon = screen.getByRole('img');
+    const pageTitle = screen.getByText('Foods');
+    const srcMagnifierIcon = magnifierIcon.getAttribute('src');
+    const srcUserIcon = userIcon.getAttribute('src');
+    expect(pageTitle).toBeInTheDocument();
+    expect(userIcon).toBeInTheDocument();
+    expect(magnifierIcon).toBeInTheDocument();
+    expect(srcMagnifierIcon).toBe(searchIcon);
+    expect(srcUserIcon).toBe(profileIcon);
   });
 
   it('', () => {
