0c14e0bd040d3bdb4ba8b971124a6cf7de9f72b7
Juliane Marques <mqs.juliane@gmail.com>
2022-01-29 08:59:20 -0300

    feat: adding some tests for login page

--- src/tests/login.test.js
+++ src/tests/login.test.js
@@ -1,2 +1,46 @@
 import React from 'react';
-import { render } from '@testing-library/react';
+import { render, screen } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
+import '@testing-library/jest-dom';
+import App from '../App';
+import Login from '../pages/Login/index';
+import renderWithRouter from '../helpers/renderWithRouter';
+
+const EMAIL_INPUT = 'email-input';
+const PASSWORD_INPUT = 'password-input';
+const BUTTON_LOGIN = 'login-submit-btn';
+
+describe('Login Page Tests', () => {
+  const emailInput = screen.queryByTestId(EMAIL_INPUT);
+  const passwordInput = screen.queryByTestId(PASSWORD_INPUT);
+  const btnLogin = screen.queryByTestId(BUTTON_LOGIN);
+
+  it('tests if the icon appears on the screen', () => {
+    render(<App />);
+  });
+
+  it('tests if the elements are present on the screen', () => {
+    render(<App />);
+    expect(emailInput).toBeInTheDocument();
+    expect(passwordInput).toBeInTheDocument();
+    expect(btnLogin).toBeInTheDocument();
+  });
+
+  it('tests if the button is disabled if there is a invalid email', () => {
+    render(<App />);
+    userEvent.type(emailInput, 'potato.com');
+    expect(btnLogin).toHaveAttribute('disabled');
+  });
+
+  it('test if the email and password validation is correct', () => { // testar se os email e senha tao rolando
+    render(<App />);
+  });
+
+  it('test if the route is correct when the button is clicked', () => { // testar se o vai pro caminho '/'
+    render(<App />);
+  });
+});
+
+describe('', () => {
+
+});
