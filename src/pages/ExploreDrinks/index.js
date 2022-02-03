15f5ed3a407342429dcc476550e10165a8845381
Juliane Marques <mqs.juliane@gmail.com>
2022-02-03 16:23:38 -0300

    feat: implementing from 70 until 73 req on foods and drinks explore

--- src/pages/ExploreDrinks/index.js
+++ src/pages/ExploreDrinks/index.js
@@ -1,4 +1,5 @@
 import React, { useContext, useEffect } from 'react';
+import { Link } from 'react-router-dom';
 import Header from '../../components/Header';
 import Footer from '../../components/Footer';
 import RecipesContext from '../../context/RecipesContext';
@@ -9,9 +10,24 @@ function ExploreDrinks() {
     setTitle('Explore Drinks');
     setBtnSearchIcon(false);
   }, [setTitle, setBtnSearchIcon]);
+
   return (
     <div>
       <Header />
+      <Link to="/explore/drinks/ingredients">
+        <button
+          type="button"
+          data-testid="explore-by-ingredient"
+        >
+          By Ingredient
+        </button>
+      </Link>
+      <button
+        type="button"
+        data-testid="explore-surprise"
+      >
+        Surprise me!
+      </button>
       <Footer />
     </div>
   );
--- src/pages/ExploreFood/index.js
+++ src/pages/ExploreFood/index.js
@@ -1,4 +1,5 @@
 import React, { useContext, useEffect } from 'react';
+import { Link } from 'react-router-dom';
 import Header from '../../components/Header';
 import Footer from '../../components/Footer';
 import RecipesContext from '../../context/RecipesContext';
@@ -12,6 +13,28 @@ function ExploreFood() {
   return (
     <div>
       <Header />
+      <Link to="/explore/foods/ingredients">
+        <button
+          type="button"
+          data-testid="explore-by-ingredient"
+        >
+          By Ingredient
+        </button>
+      </Link>
+      <Link to="/explore/foods/nationalities">
+        <button
+          type="button"
+          data-testid="explore-by-nationality"
+        >
+          By Nationality
+        </button>
+      </Link>
+      <button
+        type="button"
+        data-testid="explore-surprise"
+      >
+        Surprise me!
+      </button>
       <Footer />
     </div>
   );
