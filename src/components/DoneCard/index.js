f3046a42ce1f8d791dd11b2f892a7013f9b98db1
Juliane Marques <mqs.juliane@gmail.com>
2022-02-08 18:41:48 -0300

    feat: implementing the beggining of the done recipes page

---
+++ src/components/DoneCard/index.js
@@ -0,0 +1,37 @@
+import React from 'react';
+
+function DoneCard() {
+
+}
+
+
+  <>
+    <div>
+      <img
+        alt="img"
+        data-testid={ `${index}-horizontal-image` }
+      />
+    </div>
+    <section>
+      <p
+        data-testid={ `${index}-horizontal-top-text` }
+      />
+      <p
+        data-testid={ `${index}-horizontal-name` }
+      />
+    </section>
+    <p
+      data-testid={ `${index}-horizontal-done-date` }
+    />
+    <input
+      type="image"
+      data-testid={ `${index}-horizontal-share-btn` }
+      alt="share"
+    />
+    <span
+      data-testid={ `${index}-${elemento}-horizontal-tag` }
+    />
+  </>,
+
+
+export default DoneCard;
