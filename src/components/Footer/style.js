6a276089711c22d3252dce6619220ab95a974bf7
Laura <la_12_msn@hotmail.com>
2022-02-07 22:12:18 -0300

    fix: footer css

--- src/components/Footer/style.js
+++ src/components/Footer/style.js
@@ -3,6 +3,8 @@ import styled from 'styled-components';
 export const FooterContainer = styled.footer`
   width: 100%;
   height: 100px;
+  position: fixed;
+  bottom: 0;
 `;
 
 export const InnerFooterContainer = styled.footer`
